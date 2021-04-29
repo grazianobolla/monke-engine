09f0486a5587a89988bb10db7bab5d7b11c063db
Graziano Bolla <grazianobolla12@gmail.com>
2021-04-29 20:01:04 -0300

    cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -4,18 +4,21 @@
 
 class Game : public mk::Engine
 {
+    //create sprite
     mk::Sprite sprite;
 
     void start()
     {
+        //load texture on the engine
         mk::ResourceLoader::load_texture("textures/sheet.png", "sheet_texture");
-        mk::ResourceLoader::log_resources();
 
-        sprite.load("sheet_texture", {0, 32, 32, 32});
+        //bind the texture to the sprite and select a rectangle, top-left is 0, 0
+        sprite.load("sheet_texture", {0, 0, 32, 32});
     }
 
     void update(float delta)
     {
+        //draw the sprite at the current mouse position
         sprite.draw(input.mouse_position);
     }
 };
