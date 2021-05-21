657b0e37bdf6e90b142f4f42cf93fa5f5282bea5
Graziano Bolla <grazianobolla12@gmail.com>
2021-05-21 08:08:52 -0300

    added main example

--- src/main.cpp
+++ src/main.cpp
@@ -2,26 +2,41 @@
 #include "resource_loader.h"
 
 using namespace mk;
+using namespace glm;
 
 class Game : public Engine
 {
-    Sprite sprite, sprite1;
+    Sprite sprite;
+    vec2 display_size;
 
     void start()
     {
         //load the texture into memory
         ResourceLoader::load_texture("textures/sheet.png", "sheet");
-        ResourceLoader::load_texture("textures/happy.png", "happy");
+
+        //attach texture to the sprite
         sprite.load("sheet");
-        sprite1.load("happy");
+
+        //get the display size
+        display_size = display.get_size();
     }
 
     void update(float delta) {}
 
     void render()
     {
-        renderer.draw(sprite, input.mouse_position);
-        renderer.draw(sprite1, {100, 100});
+        //iterate the screen
+        for (float x = 0; x < display_size.x; x += 32)
+            for (float y = 0; y < display_size.y; y += 32)
+            {
+                //randomly set the sprite TextureRect
+                //each frame is 32 pixels, and they are aligned on the X axis
+                int frame = (rand() % 4) * 32;
+                sprite.texture_rect = {frame, 0, 32, 32};
+
+                //send the draw call
+                renderer.draw(sprite, {x, y});
+            }
     }
 };
 
