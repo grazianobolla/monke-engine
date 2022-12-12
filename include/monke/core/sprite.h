b316fb3bd769ae4df2a73b713f37cdea6be8dc26
Graziano Bolla <grazianobolla12@gmail.com>
2022-12-12 06:56:30 -0300

    added angle property to sprite (wip)

--- include/monke/core/sprite.h
+++ include/monke/core/sprite.h
@@ -10,10 +10,11 @@ namespace mk
         mk::Vector2 position, scale;
         mk::Rectf texture_rect;
         mk::Texture *texture_ptr;
+        float angle = 0;
 
         bool loaded = false;
 
         Sprite() {}
-        void load(const char *texture_path, mk::Vector2 position = {0, 0}, mk::Vector2 scale = {1, 1}, mk::Rectf texture_rect = {0, 0, 0, 0});
+        void load(const char *texture_path, mk::Vector2 position = {0, 0}, float angle = 0, mk::Vector2 scale = {1, 1}, mk::Rectf texture_rect = {0, 0, 0, 0});
     };
 };
--- src/core/sprite.cpp
+++ src/core/sprite.cpp
@@ -2,11 +2,12 @@
 #include "monke/core/resource_loader.h"
 #include "monke/core/log.h"
 
-void mk::Sprite::load(const char *texture_path, mk::Vector2 _position, mk::Vector2 _scale, mk::Rectf _rect)
+void mk::Sprite::load(const char *texture_path, mk::Vector2 _position, float _angle, mk::Vector2 _scale, mk::Rectf _rect)
 {
     this->texture_ptr = mk::ResourceLoader::load_texture(texture_path);
     this->position = _position;
     this->scale = _scale;
+    this->angle = _angle;
 
     if (this->texture_ptr == nullptr)
     {
