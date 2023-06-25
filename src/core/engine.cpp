7550eb11c02d3fad0d1998f6e7963d8f00e3758f
Graziano Bolla <grazianobolla12@gmail.com>
2023-06-25 20:50:20 -0300

    delta type fix

--- src/core/engine.cpp
+++ src/core/engine.cpp
@@ -24,7 +24,7 @@ void mk::Engine::run(int width, int height, const char *title)
     this->start(); // virtual function
 
     double now = 0, last_time = 0;
-    float delta = 0;
+    double delta = 0;
 
     while (glfwWindowShouldClose(display.window) == false)
     {
@@ -36,7 +36,8 @@ void mk::Engine::run(int width, int height, const char *title)
 
         mk::ImGUIHelper::new_frame();
 
-        this->compute_logic(delta);
+        float f_delta = static_cast<float>(delta);
+        this->compute_logic(f_delta);
         this->compute_rendering();
 
         std::this_thread::sleep_for(std::chrono::milliseconds(1)); // TODO: handle this
